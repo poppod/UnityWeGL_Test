5799eb0211db18dac83ba40676303a72aa5f5c09
poppod <pop.podmenyou@gmail.com>
2022-11-01 14:37:20 +0700

    first commit

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Wjames-Wirtual-0.1";
+const contentToCache = [
+    "Build/UnityWebGL_Test.loader.js",
+    "Build/UnityWebGL_Test.framework.js.unityweb",
+    "Build/UnityWebGL_Test.data.unityweb",
+    "Build/UnityWebGL_Test.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
